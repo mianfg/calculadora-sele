d0b85fbeeb8a83901bb3d9f191800abef79e0264
mianfg <mianfg19@gmail.com>
2020-04-18 12:13:33 +0200

    Cache busting

--- static/js/app_v1.3.js
+++ static/js/app_v1.3.js
@@ -9,6 +9,9 @@
 
 $(document).ready(function () {
     showHelper(false);
+    $(function () {
+        $('[data-toggle="popover"]').popover()
+    })
 });
 
 /**
@@ -19,9 +22,9 @@ $(document).ready(function () {
  *      -2 if input is empty
  */
 function parse(input) {
-    if (input=="") return -2;
-    value = parseFloat(input.replace(",","."));
-    if ( value < 0 || value > 10 )
+    if (input == "") return -2;
+    value = parseFloat(input.replace(",", "."));
+    if (value < 0 || value > 10)
         return -1;
     else
         return value;
@@ -38,9 +41,9 @@ function parse(input) {
 function parseRequire(dom_id) {
     val = parse(document.getElementById(dom_id).value);
     if (val < 0)
-        $("#"+dom_id).addClass("is-invalid");
+        $("#" + dom_id).addClass("is-invalid");
     else
-        $("#"+dom_id).removeClass("is-invalid");
+        $("#" + dom_id).removeClass("is-invalid");
 
     return val;
 }
@@ -56,9 +59,9 @@ function parseRequire(dom_id) {
 function parseOptional(dom_id) {
     val = parse(document.getElementById(dom_id).value);
     if (val == -1)
-        $("#"+dom_id).addClass("is-invalid");
+        $("#" + dom_id).addClass("is-invalid");
     else
-        $("#"+dom_id).removeClass("is-invalid");
+        $("#" + dom_id).removeClass("is-invalid");
 
     return val;
 }
@@ -92,41 +95,41 @@ function setHelper(arr) {
     document.getElementById("helper-4").innerHTML = '<p class="mb-1"><strong><i class="fa fa-search mr-2"></i>En tu caso:</strong></p>';
     document.getElementById("helper-5").innerHTML = '<p class="mb-1"><strong><i class="fa fa-search mr-2"></i>En tu caso:</strong></p>';
     document.getElementById("helper-6").innerHTML = '<p class="mb-1"><strong><i class="fa fa-search mr-2"></i>En tu caso:</strong></p>';
-    
+
     over_five = []
-    if ( arr.length == 0 )
+    if (arr.length == 0)
         document.getElementById("helper-3").innerHTML += '<span>No te has evaluado de ninguna asignatura específica.</span>'
-    for ( i = 0; i < arr.length; i++ ) {
-        document.getElementById("helper-3").innerHTML += '<span class="badge badge-light">'+arr[i][0].toFixed(2).toString()+'</span>';
-        if ( arr[i][0] >= 5 ) {
+    for (i = 0; i < arr.length; i++) {
+        document.getElementById("helper-3").innerHTML += '<span class="badge badge-light">' + arr[i][0].toFixed(2).toString() + '</span>';
+        if (arr[i][0] >= 5) {
             over_five.push(arr[i])
         }
-        if ( i < arr.length - 1 )
+        if (i < arr.length - 1)
             document.getElementById("helper-3").innerHTML += '<span>, </span>';
     }
 
     esp = []
-    if ( over_five.length == 0 ) {
+    if (over_five.length == 0) {
         document.getElementById("helper-4").innerHTML += '<span>No tienes ninguna asignatura en fase específica con puntuación mayor o igual a 5, pero ¡no te preocupes! ¡El resto también cuenta y mucho! :)</span>'
         document.getElementById("helper-5").innerHTML += '<span>No tienes ninguna asignatura en fase específica con puntuación mayor o igual a 5, pero ¡no te preocupes! ¡El resto también cuenta y mucho! :)</span>'
         document.getElementById("helper-6").innerHTML += '<span>No tienes ninguna asignatura en fase específica con puntuación mayor o igual a 5, pero ¡no te preocupes! ¡El resto también cuenta y mucho! :)</span>'
     }
-    for ( i = 0; i < over_five.length; i++ ) {
-        document.getElementById("helper-4").innerHTML += '<span class="badge badge-light">'+over_five[i][0].toFixed(2).toString()+'</span>';
-        document.getElementById("helper-5").innerHTML += '<span>' + over_five[i][0].toFixed(2).toString() + ' × ' + over_five[i][1].toFixed(2).toString() + ' = </span><span class="badge badge-light">'+(over_five[i][0]*over_five[i][1]).toFixed(3).toString()+'</span>' 
-        if ( i < over_five.length - 1 ) {
+    for (i = 0; i < over_five.length; i++) {
+        document.getElementById("helper-4").innerHTML += '<span class="badge badge-light">' + over_five[i][0].toFixed(2).toString() + '</span>';
+        document.getElementById("helper-5").innerHTML += '<span>' + over_five[i][0].toFixed(2).toString() + ' × ' + over_five[i][1].toFixed(2).toString() + ' = </span><span class="badge badge-light">' + (over_five[i][0] * over_five[i][1]).toFixed(3).toString() + '</span>'
+        if (i < over_five.length - 1) {
             document.getElementById("helper-4").innerHTML += '<span>, </span>';
             document.getElementById("helper-5").innerHTML += '<span>, </span>';
         }
-        esp.push(over_five[i][0]*over_five[i][1]);
+        esp.push(over_five[i][0] * over_five[i][1]);
     }
 
     max = getEspecificas(esp);
-    if ( max.length > 0 )
-        document.getElementById("helper-6").innerHTML += '<span class="badge badge-danger">'+max.reduce((a,b)=>a+b,0).toFixed(3).toString()+'</span><span> = </span>';
-    for ( i = 0; i < max.length; i++ ) {
-        document.getElementById("helper-6").innerHTML += '<span class="badge badge-light">'+max[i].toFixed(3).toString()+'</span>';
-        if ( i < max.length - 1 )
+    if (max.length > 0)
+        document.getElementById("helper-6").innerHTML += '<span class="badge badge-danger">' + max.reduce((a, b) => a + b, 0).toFixed(3).toString() + '</span><span> = </span>';
+    for (i = 0; i < max.length; i++) {
+        document.getElementById("helper-6").innerHTML += '<span class="badge badge-light">' + max[i].toFixed(3).toString() + '</span>';
+        if (i < max.length - 1)
             document.getElementById("helper-6").innerHTML += '<span> + </span>';
     }
 }
@@ -136,14 +139,18 @@ function setHelper(arr) {
  * @param {Boolean} b true is show, false is hide
  */
 function showHelper(b) {
-    for ( i = 2; i <= 6; i++ ) {
-        if (b) $("#helper-"+i.toString()).show();
-        else $("#helper-"+i.toString()).hide();
+    for (i = 2; i <= 6; i++) {
+        if (b) $("#helper-" + i.toString()).show();
+        else $("#helper-" + i.toString()).hide();
     }
     if (b) document.getElementById("helper-1").innerHTML = '<p class="mb-1"><strong><i class="fa fa-search mr-2"></i>Comprobar nota:</strong> te explicaremos cómo hemos calculado tu nota, paso a paso.<br><span class="badge badge-success" id="helper-n">--.---</span><span> = 0.6 × </span><span class="badge badge-info" id="helper-mb">.</span><span> + 0.4 × </span><span class="badge badge-warning" id="helper-t0">.</span><span> + </span><span class="badge badge-danger" id="helper-e">Nota de la Fase Específica</span></p>';
     else document.getElementById("helper-1").innerHTML = '<strong><i class="fa fa-search mr-2"></i>Comprobar nota:</strong> Introduce tus notas y luego pulsa en el botón <strong>Calcular</strong>. Vuelve aquí para una explicación detallada de cómo la hemos obtenido con tus calificaciones.';
 }
 
+/**
+ * 
+ * @param {Boolean} b true is show, false is hide
+ */
 function calculateCall(b) {
     if (b) {
         document.getElementById("calculate-call").innerHTML = "COMPRUEBA TU NOTA";
@@ -163,19 +170,19 @@ document.getElementById("calcular").onclick = function () {
     showHelper(true);
     calculate = true;
     has_coof = false;
-    
+
     media_bach = parseRequire("media-bach");
     if (media_bach < 0) calculate = false;
 
     document.getElementById("helper-mb").innerHTML = media_bach.toFixed(2);
-    
+
     troncales = 0; n = 4.0;
     // comprobar primero si hay lengua cooficial
     coof = parseOptional("troncal-c");
-    if ( coof != -2 ) {
+    if (coof != -2) {
         has_coof = true;
         n = 5.0;
-        troncales += coof/n;
+        troncales += coof / n;
         document.getElementById("coof-n").innerHTML = " ) / 5";
         $("#helper-troncal-c").show()
         $("#sum-troncal-c").show()
@@ -186,44 +193,50 @@ document.getElementById("calcular").onclick = function () {
         $("#sum-troncal-c").hide()
     }
 
-    for ( i = 1; i <= 4; i++ ) {
+    for (i = 1; i <= 4; i++) {
         dom_id = "troncal-" + i.toString();
         if (i == 4) dom_id = "especifica-4";
         value = parseRequire(dom_id);
-        document.getElementById("helper-"+dom_id).innerHTML = value.toFixed(2);
+        document.getElementById("helper-" + dom_id).innerHTML = value.toFixed(2);
         if (value < 0) calculate = false;
-        troncales += value/n;
+        troncales += value / n;
     }
 
     document.getElementById("helper-t").innerHTML = troncales.toFixed(3);
 
     especificas = []
     especificas_helper = []
-    for ( i = 1; i <= 4; i++ ) {
+    for (i = 1; i <= 4; i++) {
         dom_id = "especifica-" + i.toString();
         value = parseOptional(dom_id);
         if (i == 4) value = parseRequire(dom_id);
         if (value == -1) calculate = false;
-        else if (value > 0) especificas_helper.push([value, ponder[i-1]])
-        if (value >= 5) especificas.push(value*ponder[i-1]);
+        else if (value > 0) especificas_helper.push([value, ponder[i - 1]])
+        if (value >= 5) especificas.push(value * ponder[i - 1]);
     }
 
     setHelper(especificas_helper);
-    nota_especifica = getEspecificas(especificas).reduce((a,b)=>a+b,0);
-    
+    nota_especifica = getEspecificas(especificas).reduce((a, b) => a + b, 0);
+
     if (calculate) {
         showHelper(true);
-        nota = media_bach*0.6 + troncales*0.4 + nota_especifica;
+        nota = media_bach * 0.6 + troncales * 0.4 + nota_especifica;
         document.getElementById("nota-evau").innerHTML = nota.toFixed(3);
         document.getElementById("helper-n").innerHTML = nota.toFixed(3);
         document.getElementById("helper-mb").innerHTML = media_bach.toFixed(2);
         document.getElementById("helper-e").innerHTML = nota_especifica.toFixed(3);
         document.getElementById("helper-t0").innerHTML = troncales.toFixed(3);
+        $("#calcular").addClass("btn-primary");
+        $("#calcular").removeClass("btn-danger");
+        $("#error-pop").hide();
         calculateCall(true);
-        window.scrollTo(0,0);
+        window.scrollTo(0, 0);
     } else {
         document.getElementById("nota-evau").innerHTML = "--.---";
         showHelper(false);
+        $("#calcular").removeClass("btn-primary");
+        $("#calcular").addClass("btn-danger");
+        $("#error-pop").show();
         calculateCall(false);
     }
 }
@@ -232,17 +245,17 @@ document.getElementById("calcular").onclick = function () {
 var ponder = [0.1, 0.1, 0.1, 0.1]
 
 // generate pondering button listeners for click event
-for ( var i = 1; i <= 4; i++ ) {
+for (var i = 1; i <= 4; i++) {
     let local_i = i;
-    for ( var j = 1; j <= 3; j++ ) {
+    for (var j = 1; j <= 3; j++) {
         let local_j = j;
-        let dom_id = "especifica-"+i.toString()+"-p"+j.toString();
-        document.getElementById(dom_id).onclick = function() {
-            ponder[local_i-1] = 0.1 + 0.05*(local_j-1);
-            $("#"+dom_id).addClass("active");
-            for ( k = 1; k <= 3; k++ )
+        let dom_id = "especifica-" + i.toString() + "-p" + j.toString();
+        document.getElementById(dom_id).onclick = function () {
+            ponder[local_i - 1] = 0.1 + 0.05 * (local_j - 1);
+            $("#" + dom_id).addClass("active");
+            for (k = 1; k <= 3; k++)
                 if (k != local_j)
-                    $("#especifica-"+local_i.toString()+"-p"+k.toString()).removeClass("active");
+                    $("#especifica-" + local_i.toString() + "-p" + k.toString()).removeClass("active");
         }
     }
 }
